ed69b9acfeb1ee19d902b61838dc5acf1a20c8ee
Aite Aigbe <aite@nabla.ooo>
2021-07-18 02:18:53 -0400

    stable

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import './App.css';
 import SvgNabla from './svg';
-import React, { useRef, useState, Suspense } from 'react';
+import CD from './Canvas_CD';
+import React, { useRef, useState, useEffect , Suspense } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { softShadows, OrbitControls, useGLTF, PerspectiveCamera, CameraShake  } from '@react-three/drei'
-import { EffectComposer, DepthOfField, SMAA  , Noise, Vignette } from '@react-three/postprocessing';
+import { EffectComposer, DepthOfField, Noise, Vignette, ChromaticAberration   } from '@react-three/postprocessing';
 import { BlendFunction, UnrealBloomPass } from 'postprocessing';
 import { useSpring, a } from '@react-spring/three';
 import reactDom from 'react-dom';
+
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
+ function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+}
+
 
 // HTML 
 const Filler = (props) => {
@@ -44,7 +69,7 @@ const Header = () =>{
           <ProgressBar  />
           <ul>
             <li>
-              <a href="https://nablaooo.gumroad.com/" target="_blank" >Store</a>
+              <a href="https://nablaooo.gumroad.com/" target="_blank">Store</a>
             </li>
             <li>
               <a href="/" >Contact</a>
@@ -56,9 +81,6 @@ const Header = () =>{
       </header>
   );
 };
-
-
-
 
 // Canvas
 softShadows();
@@ -74,54 +96,45 @@ function Sand(props) {
     </group>
   )
 }
-function CD(props) {
-  const group = useRef()
 
-  const [hovered, setHovered] = useState(false);
-  const { nodes, materials } = useGLTF('Canvas_CD.glb')
-
-  useFrame(() => {
-    group.current.rotation.y += 0.02;
-   });
-
+function Plane({ color, ...props }) {
   return (
-    <group  {...props} dispose={null}>
-      <group ref={group} position={[0, 800, 2000]}>
-        <group position={[0.47, -57.27, -3.71]} scale={1.5}>
-          <mesh geometry={nodes.Sphere2.geometry} material={nodes.Sphere2.material} position={[5.28, 57.74, -265.23]} />
-          <mesh geometry={nodes.Sphere1.geometry} material={nodes.Sphere1.material} position={[306.5, 57.74, 134.46]} />
-          <mesh geometry={nodes.Sphere.geometry} material={nodes.Sphere.material} position={[-296.7, 57.74, 134.46]} />
-        </group>
-      </group>
-    </group>
+    <mesh receiveShadow castShadow {...props}>
+      <boxBufferGeometry />
+      <meshStandardMaterial color={color} />
+    </mesh>
   )
 }
 
-
-
+//App
 function App() {
   
-  const myCamera = useState();
+  const myCamera = useRef();
+  const { height, width } = useWindowDimensions();
+  console.log(width, height);
   return (
     <>
     <Header />
-    <Canvas colorManagement>
-      <PerspectiveCamera makeDefault ref={myCamera} position={[900,750,-1000]} far={800000} fov={50} rotation={[-Math.PI, 0, 0]} />
+    <Canvas shadows colorManagement>
+      <PerspectiveCamera makeDefault ref={myCamera} position={[900,750,-1000]}  fov={50} aspect={width / height} far={80000} />
       <gridHelper />
       <fog attach="fog" args={[0x848484, 1000, 40]} />
       <directionalLight  intensity={3} color={0x848484 } position={[9000, 6000, -5000]} />
       <Suspense fallback={null}>
         <Sand />
         <CD />
-      
+        <Plane color="#736fbd" position-y={-250} scale={[16000.2, 200, 10000]} />
       <EffectComposer>
-      <SMAA />
-        {/* <DepthOfField focusDistance={50} focalLength={2} bokehScale={5} height={480} /> */}
+      <ChromaticAberration
+    blendFunction={BlendFunction.NORMAL} // blend mode
+    offset={[0.00008, 0.0008]} // color offset
+  />
+        {/* <DepthOfField focusDistance={5} focalLength={5} bokehScale={10} height={480} /> */}
         <Noise opacity={.059} />
         <Vignette eskil={false} offset={0.1} darkness={1} />
       </EffectComposer>
       </Suspense>
-      <OrbitControls enablePan={false} enableRotate={false} enableZoom={false} camera={myCamera.current}/>
+      <OrbitControls enablePan={true} enableRotate={true} enableZoom={true} camera={myCamera.current}/>
     </Canvas>
     </>
   );
