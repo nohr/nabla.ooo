d7b4463d529596feeaeb8a7aaa62551c4faea3dc
Aite Aigbe <aite@nabla.ooo>
2021-07-29 22:46:10 -0400

    latest build

--- src/components/Stream/Stream.jsx
+++ src/components/Stream/Stream.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
-import HorizontalScroll from 'react-scroll-horizontal'
 import "./Stream.css";
 
 
 
 class Stream extends Component {
   render() {
-    const child = { width: `300em`, height: `100vw`}
     return (
       <div className="container">
         
