f96460c7243d4a20ac905ba8be1fb628e40e85b4
Aite Aigbe <aite@nabla.ooo>
2022-01-25 23:22:47 -0500

    return to strict mode

--- src/index.js
+++ src/index.js
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom';
 import './App.css';
 import App from './App';
 
+
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
