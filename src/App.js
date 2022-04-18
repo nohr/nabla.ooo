50155300195eb3cd0dcc621218ead25877ddc35e
Aite Aigbe <aite@nabla.ooo>
2022-04-18 14:01:00 -0400

    detect gpu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import React from 'react'
 import CanvasComp from './components/Canvas/Canvas'
 import UI from './components/UI/UI'
 import { state } from './components/UI/state'
 import { db, GetWorks } from './firebase'
+import { getGPUTier } from 'detect-gpu';
 
 //App 
 function App() {
+
+  // GPU
+  (async () => {
+    const gpuTier = await getGPUTier();
+
+    if (gpuTier.tier >= 3) {
+      state.paused = false;
+    }
+
+    console.log(gpuTier);
+    // Example output:
+    // {
+    //   "tier": 1,
+    //   "isMobile": false,
+    //   "type": "BENCHMARK",
+    //   "fps": 21,
+    //   "gpu": "intel iris graphics 6100"
+    // }
+  })();
+
   //Load project titles
   GetWorks(db);
   //Change the theme based on user preference
